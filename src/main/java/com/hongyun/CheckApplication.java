129ab66c7f4ad876ef695badac3aabb5a538ad5f
ChenHongYun <851891179@qq.com>
2024-07-10 22:21:13 +0800

    init

---
+++ src/main/java/com/hongyun/CheckApplication.java
@@ -0,0 +1,14 @@
+package com.hongyun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckApplication.class, args);
+        System.out.println("start successfully !");
+    }
+
+}
