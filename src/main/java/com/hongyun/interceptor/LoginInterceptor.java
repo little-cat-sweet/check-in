e1c472c8d666f027364a54ad751678ba588237d3
Mark <wye87444@gmail.com>
2026-05-23 16:42:42 +0800

    complete login status is 401 logic.

--- src/main/java/com/hongyun/interceptor/LoginInterceptor.java
+++ src/main/java/com/hongyun/interceptor/LoginInterceptor.java
@@ -1,24 +1,18 @@
 package com.hongyun.interceptor;
 
-import com.hongyun.dto.vo.User;
 import com.hongyun.util.UserHolder;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Objects;
 
 public class LoginInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-
-        User userDto = UserHolder.getUser();
-        if(Objects.isNull(userDto)){
-            response.setStatus(404);
-            response.setStatus(HttpStatus.UNAUTHORIZED.value());
-            response.getWriter().write("UnAuthorization");
+        if (UserHolder.getUser() == null) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write("Unauthorized");
             return false;
         }
         return true;
